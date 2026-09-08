724397c24382d23fe98a4007a40a695cbb721578
Kiwikorna <180771659+Kiwikorna@users.noreply.github.com>
2026-09-08 17:28:35 +0300

    perf(bullets): reduce calls instatiate and destoroy
    I created an object pool. It controls creating, enabling, and disabling objects. It reduced calls instatiate and destroy for bullets.

--- Assets/Script/BulletController.cs
+++ Assets/Script/BulletController.cs
@@ -8,17 +8,16 @@ namespace Bullet.BulletController
     public class BulletController : MonoBehaviour
     {
         [SerializeField] private BulletSO attackProperties;
-
+        
         // Update is called once per frame
         void Update()
         {
-            if(DistanceToDestroyBullet())
-                Destroy(gameObject);
             transform.Translate(Vector2.up * (attackProperties.speed * Time.deltaTime));
         }
     
-        private bool DistanceToDestroyBullet() => Mathf.Abs(transform.localPosition.y - attackProperties.range) <= 0.3f;
-        public GameObject GetBullet() => attackProperties.spawnPrefabForAttack;
+        public bool DistanceToDestroyBullet() => Mathf.Abs(transform.localPosition.y - attackProperties.range) <= 0.3f;
+        public GameObject GetBulletPrefab() => attackProperties.spawnPrefabForAttack;
     }
 
+    
 }
